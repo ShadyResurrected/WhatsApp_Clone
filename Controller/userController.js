c809c5f784513c427a8206b54b4cae4cdb2fda8c
ShadyResurrected <cchandra554@gmail.com>
2022-12-30 15:13:43 +0530

    add,users route

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,3 +1,30 @@
-export const addUser = (request,response) => {
-    console.log(request.body)
+import user from "../Model/User.js";
+
+export const addUser = async (request, response) => {
+  try {
+    let exist = await user.findOne({ sub: request.body.sub });
+    if (exist) {
+      response.status(200).json({ msg: "User already exists" });
+      return;
+    }
+
+    const newUser = new user(request.body);
+
+    // To save the user in database
+    await newUser.save();
+    return response.status(200).json(newUser);
+  } catch (error) {
+    return response.status(500).json(error.message);
+  }
+};
+
+
+export const getUsers = async(request,response) => {
+  try {
+    // Providing it with an empty object means we want to fetch all the data
+    const users = await user.find({})
+    return response.status(200).json(users)
+  } catch (error) {
+    return response.status(500).json(error.message)
+  }
 }
--- Routes/route.js
+++ Routes/route.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { addUser } from "../Controller/userController.js";
+import { addUser, getUsers } from "../Controller/userController.js";
 
 const route = express.Router();
 
 // Adding a user
 route.post("/add", addUser);
+route.get("/users", getUsers);
 
 export default route;
