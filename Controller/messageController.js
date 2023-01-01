c83d31a83b6a5afc39ef09a73e1b78ddfaee7f44
ShadyResurrected <cchandra554@gmail.com>
2023-01-01 14:31:36 +0530

    get all messages from db

--- Controller/messageController.js
+++ Controller/messageController.js
@@ -16,3 +16,12 @@ export const newMessage = async (request, response) => {
     return response.status(500).json(error.message);
   }
 };
+
+export const getMessages = async(request,response) => {
+  try {
+    const messages = await message.find({conversationId : request.params.id})
+    return response.status(200).json(messages)
+  } catch (error) {
+    return response.status(500).json(error.message)
+  }
+}
--- Routes/route.js
+++ Routes/route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { newConversation,getConversation } from "../Controller/conversationController.js";
-import { newMessage } from "../Controller/messageController.js";
+import { getMessages, newMessage } from "../Controller/messageController.js";
 import { addUser, getUsers } from "../Controller/userController.js";
 
 
@@ -12,5 +12,6 @@ route.get("/users", getUsers);
 route.post("/conversation/add", newConversation);
 route.post("/conversation/get", getConversation);
 route.post('/message/add', newMessage)
+route.get('/message/get/:id', getMessages)
 
 export default route;
