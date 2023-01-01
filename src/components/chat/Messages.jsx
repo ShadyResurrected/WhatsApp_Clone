a560157a392d59903ac08d0f283c404710470dfb
ShadyResurrected <cchandra554@gmail.com>
2023-01-01 14:32:16 +0530

    Fetching and displaying messages

--- src/components/chat/Messages.jsx
+++ src/components/chat/Messages.jsx
@@ -1,53 +1,76 @@
-import { Box,styled } from '@mui/material'
-import {React,useContext, useState} from 'react'
+import { Box, styled } from "@mui/material";
+import { React, useContext, useState, useEffect } from "react";
 
 import { AccountContext } from "../../context/AccountProvider";
-import { newMessage } from '../../service/api';
+import { getMessages, newMessage } from "../../service/api";
 
-import Footer from './Footer'
+import Footer from "./Footer";
+import Message from "./Message";
 
 const Wrapper = styled(Box)`
-background-image: url(${'https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png'});
-background-size: 50%;
-`
+  background-image: url(${"https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png"});
+  background-size: 50%;
+`;
 
 const Component = styled(Box)`
-height: 80vh;
-overflow-y: scroll;
+  height: 80vh;
+  overflow-y: scroll;
+`;
+
+const Container = styled(Box)`
+padding: 1px 80px;
 `
 
-const Messages = ({person,conversation}) => {
+const Messages = ({ person, conversation }) => {
+  const [value, setValue] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [newMessageFlag, setNewMessageFlag] = useState(false)
 
-  const [value, setValue] = useState('')
+  const { account } = useContext(AccountContext);
 
-  const {account} = useContext(AccountContext)
+  useEffect(() => {
+    const getMessageDetails = async () => {
+      let data = await getMessages(conversation._id);
+      setMessages(data);
+    };
+    conversation._id && getMessageDetails();
+  }, [person._id, conversation._id,newMessageFlag]);
 
-  const sendText = async(e) => {
+  const sendText = async (e) => {
     const code = e.keyCode || e.which; // this retrieves the code of pressed key
     // code 13 is of enter key
-    if(code === 13){
+    if (code === 13) {
       // making a model for message
       let message = {
-        senderId : account.sub,
-        receiverId : person.sub,
-        conversationId : conversation._id, // id from mongoDB
-        type : 'text',
-        text : value
-      }
+        senderId: account.sub,
+        receiverId: person.sub,
+        conversationId: conversation._id, // id from mongoDB
+        type: "text",
+        text: value,
+      };
 
-      await newMessage(message)
+      await newMessage(message);
 
       // Clearing the text area after saving the message in the database
-      setValue('')
+      setValue("");
+      // toggling the state
+      setNewMessageFlag(prev => !prev)
     }
-  }
+  };
 
   return (
     <Wrapper>
-        <Component></Component>
-        <Footer sendText={sendText} setValue={setValue} value={value}/>
+      <Component>
+        {messages &&
+          messages.map((message) => (
+            <Container>
+              <Message message={message} />
+            </Container>
+          ))}
+      </Component>
+      <Footer sendText={sendText} setValue={setValue} value={value} />
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Messages
+export default Messages;
