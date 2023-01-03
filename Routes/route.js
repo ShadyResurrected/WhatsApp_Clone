e9e6b97059ef733755a2283af213c64ced5e6138
ShadyResurrected <cchandra554@gmail.com>
2023-01-03 16:24:34 +0530

    downloading images

--- Routes/route.js
+++ Routes/route.js
@@ -1,17 +1,25 @@
-import express from "express";
-import { newConversation,getConversation } from "../Controller/conversationController.js";
-import { getMessages, newMessage } from "../Controller/messageController.js";
-import { addUser, getUsers } from "../Controller/userController.js";
+import express from 'express';
 
+import { newConversation, getConversation } from '../controller/conversationController.js';
+import { addUser, getUsers } from '../controller/userController.js';
+import { newMessage, getMessages }from '../controller/messageController.js';
+import { uploadImage, getImage } from '../controller/imageController.js';
+
+import upload from '../utils/upload.js';
 
 const route = express.Router();
 
-// Adding a user
-route.post("/add", addUser);
-route.get("/users", getUsers);
-route.post("/conversation/add", newConversation);
-route.post("/conversation/get", getConversation);
-route.post('/message/add', newMessage)
-route.get('/message/get/:id', getMessages)
+route.post('/add', addUser);
+route.get('/users', getUsers);
+
+route.post('/conversation/add', newConversation);
+route.post('/conversation/get', getConversation);
+
+route.post('/message/add', newMessage);
+route.get('/message/get/:id', getMessages);
+
+route.post('/file/upload', upload.single('file'), uploadImage);
+route.get('/file/:filename', getImage);
+
 
 export default route;
